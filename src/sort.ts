7db2de75900b303d2f3b1a441c84d26f60c7f271
Tao Hu <taohu88@gmail.com>
2025-11-08 14:28:41 -0800

    early break if there is not element swapped in bubbleSort

--- src/sort.ts
+++ src/sort.ts
@@ -3,10 +3,15 @@ export function bubbleSort<T>(a: T[], compare?: (a: T, b: T) => number): T[] {
   const cmp = compare || ((x: T, y: T):number => x < y ? -1 : x > y ? 1 : 0);
 
   for (let i = 0; i < n - 1; i++) {
+    let swapped = false;
     for (let j = 0; j < n - i - 1; j++) {
       if (cmp(a[j], a[j + 1]) > 0) {
         [a[j], a[j + 1]] = [a[j + 1], a[j]];
+        swapped = true;
       }
+    }
+    if (!swapped) {
+      break;
     }
   }
   return a;
